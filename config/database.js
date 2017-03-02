7e6e60492fdb076e9e739677f7a689ae85cc132d
ttruong89 <t.truong2009@gmail.com>
2017-03-02 16:30:19 -0600

    changes to html

---
+++ config/database.js
@@ -0,0 +1,10 @@
+// config/database.js
+module.exports = {
+    'connection': {
+        'host': 'localhost',
+        'user': 'root',
+        'password': 'maury210'
+    },
+	'database': 'lendowe_db',
+    'users_table': 'users'
+};
